682c521bf6b2158d690e3c3a3ad6253b07c99e7d
LucasCherri <80834815+LucasCherri@users.noreply.github.com>
2022-10-13 19:27:55 -0300

    Delete projeto-integrador4(spring) directory

--- projeto-integrador4(spring)/src/main/java/com/example/springprojeto/projetointegrador4/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springprojeto.projetointegrador4;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, Integer> {
-    User findByEmailAndPassword(String email, String password);
-}
